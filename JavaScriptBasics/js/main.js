aa62e997efb7b2b60da1942add8eefe928050aa7
motto-cotta <75438696+motto-cotta@users.noreply.github.com>
2021-02-18 19:35:21 +0900

    Add files via upload

---
+++ JavaScriptBasics/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const price = 1200;
+
+console.log(price > 1000);
+console.log(price < 1000);
+console.log(price >= 1000);
+console.log(price <= 1000);
+console.log(price === 1000);
+console.log(price !== 1000);
+
+console.log(Boolean(0));
+console.log(Boolean('hello'));
