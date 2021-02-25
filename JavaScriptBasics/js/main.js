5cec1686af3e4f8aa2a8433a20e476309ddfdc90
motto-cotta <75438696+motto-cotta@users.noreply.github.com>
2021-02-25 21:25:47 +0900

    Add files via upload

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -1,1 +1,11 @@
 'use strict';
+for (let i = 1; i <= 10; i++) {
+    // if (i ===4 ) {
+    //     if (i % 3 === 0) {
+    //     continue;
+    // }
+    if (i === 4) {
+        break;
+    }
+    console.log(i);
+}
