cfbe780df9eaf8401d5f9253b7e0ffe2c2b42fb1
motto-cotta <75438696+motto-cotta@users.noreply.github.com>
2021-03-02 23:19:40 +0900

    Add files via upload

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -1,15 +1,16 @@
 'use strict';
 
-function showAd() {
+function showAd(message = 'Ad') {
     console.log('----------');
-    console.log('--- Ad ---');
+    console.log(`--- ${message} ---`);
     console.log('----------');
 }
 
-showAd();
+showAd('Header ad');
 console.log('Tom is great!');
 console.log('Bob is great!');
+// showAd('Ad');
 showAd();
 console.log('Stave is great!');
 console.log('Richard is great!');
-showAd()
+showAd('Footer Ad');
