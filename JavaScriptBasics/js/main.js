a52265931f47f10f5e920a5e2d7b39e1bde01392
motto-cotta <75438696+motto-cotta@users.noreply.github.com>
2021-03-15 22:27:53 +0900

    Add files via upload

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -1,13 +1,10 @@
 'use strict';
 
 {
-    const scores = [80, 90, 40, 70]
+    const scores = [80, 90, 40, 70];
+    scores.push(60, 50);
+    scores.shift();
 
-    // console.log(`Score: ${scores[0]}`);
-    // console.log(`Score: ${scores[1]}`);
-    // console.log(`Score: ${scores[2]}`);
-
-    // for (let i = 0; i < 3; i++) {
     for (let i = 0; i < scores.length; i++) {
         console.log(`Score ${i}: ${scores[i]}`);
     }
