7e7ab0036c88ab31ff72813e85575ada58d1199a
motto-cotta <75438696+motto-cotta@users.noreply.github.com>
2021-03-31 22:04:41 +0900

    Add files via upload

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -2,10 +2,6 @@
 
 {
     const prices = [180, 190, 200];
-
-    // const updatedPrices.map((price) => {
-    //     return price + 20;
-    // });
     const updatedPrices = prices.map(price => price + 20);
     console.log(updatedPrices);
 }
