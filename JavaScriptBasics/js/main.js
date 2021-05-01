08a4fe59d60a48910bdf83896412a39abf5ab2b5
motto-cotta <75438696+motto-cotta@users.noreply.github.com>
2021-05-01 23:38:27 +0900

    Add files via upload

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -6,7 +6,7 @@
     // 0, 1, 2
   // Math.floor(Math.random() * 3)
   // 0, ..., n
-  // Math.floor(Math.random() * (n +
+  // Math.floor(Math.random() * (n
   // min, ..., max
   // Math.floor(Math.random() * (max + 1 - min))
   console.log(Math.floor(Math.random() * (6)) + 1);
