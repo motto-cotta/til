a448ac2629a46131574a517b223867a9a6da6a59
motto-cotta <75438696+motto-cotta@users.noreply.github.com>
2021-05-04 20:53:22 +0900

    Add files via upload

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
 {
-  const d = new Date();
-  // console.log(d);
-
-  console.log(`${d.getMonth() + 1} 月 ${d.getDate()} 日`)
+  const d = new Date(2019, 10);
+  d.setHours(10, 20, 30);
+  d.setDate(31);
+  d.setDate(d.getDate() + 3);
+  console.log(d);
 }
